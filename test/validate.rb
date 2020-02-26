d5d77f1ba2acac177cdd0c54490e3767c5911ff7
Christopher Kampmeier <cpkampmeier@gmail.com>
2020-02-25 19:47:03 -0600

    update source files with license header

--- test/validate.rb
+++ test/validate.rb
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 describe command('aws ec2 describe-vpcs \
                     --region us-east-1 \
                     --filters Name=tag:Name,Values=VPC-Test') do
